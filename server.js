095bd3c09e8974100657361d1e0997a331a2d5d1
Akira Kusumoto <akirakusumo10@gmail.com>
2011-12-07 20:44:44 +0900

    test

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('hello world:' + new Date());
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(8080);
-console.log('Server running');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs');
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+    console.log('connection open');
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+        socket.broadcast.emit('xx', { send: 'send' });
+    });
+//    var interval = setInterval(function() {
+//        socket.emit('news', { data:'aaaa' });
+//    },1000);
+
+});
