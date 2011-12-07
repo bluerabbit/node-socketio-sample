5a9a44425e5d0de1a4b0ae20144dc21b026da9ae
Akira Kusumoto <akirakusumo10@gmail.com>
2011-12-07 21:08:56 +0900

    aa

--- server.js
+++ server.js
@@ -1,26 +1,56 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs');
 
-app.listen(8080);
+/**
+ * Module dependencies.
+ */
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'socket.io paint'
   });
-}
-//
-//io.sockets.on('connection', function (socket) {
-//    console.log('connection open');
-//    socket.emit('news', { hello: 'world' });
-//    socket.on('my other event', function (data) {
-//        console.log(data);
-//        socket.broadcast.emit('xx', { send: 'send' });
-//    });
-//});
+});
+
+app.listen(80);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// Server
+
+var points = [];
+var io = require('socket.io').listen(app);
+
+paint = io.of('/paint').on('connection', function (socket) {
+  if (points.length > 0) {
+    for (var i in points) {
+      socket.json.emit('paint points', points[i]);
+    }
+  }
+
+  socket.on('paint points', function(data) {
+    points.push(data);
+    paint.emit('paint points', data);
+  });
+});
